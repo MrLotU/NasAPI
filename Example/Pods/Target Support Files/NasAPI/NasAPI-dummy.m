7a4bea2292efe7c8a5a61771f5320a672bf77ecb
MrLotU <j.koopman@jarict.nl>
2017-10-26 16:30:56 +0200

    Added supporting files

---
+++ Example/Pods/Target Support Files/NasAPI/NasAPI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NasAPI : NSObject
+@end
+@implementation PodsDummy_NasAPI
+@end
---
+++ Example/Pods/Target Support Files/Pods-NasAPI_Tests/Pods-NasAPI_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NasAPI_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_NasAPI_Tests
+@end
